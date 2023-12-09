6e38423324ae9135f985cbbe6fcfde47cf37bc64
Gravitar64 <gravitar@web.de>
2023-12-09 13:32:05 +0100

    YouTube2

--- day09.py
+++ day09.py
@@ -3,16 +3,17 @@ import time
 
 def load(file):
   with open(file) as f:
-    return [list(map(int,row.strip().split())) for row in f]
-  
+    return [list(map(int, row.strip().split())) for row in f]
 
-def get_next_number(sequenz,part1):
+
+def get_next_number(sequenz, part1):
   if len(set(sequenz)) == 1: return sequenz[0]
-  next_number = get_next_number([b - a for a,b in zip(sequenz, sequenz[1:])], part1)
+  next_number = get_next_number([b - a for a, b in zip(sequenz, sequenz[1:])], part1)
   return sequenz[-1] + next_number if part1 else sequenz[0] - next_number
 
-def solve(p,part1=True):
-  return sum(get_next_number(sequenz,part1) for sequenz in p)
+
+def solve(p, part1=True):
+  return sum(get_next_number(sequenz, part1) for sequenz in p)
 
 
 time_start = time.perf_counter()
