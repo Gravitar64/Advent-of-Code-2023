58f7022ec530fa3adc54b4a75c01f2a87bf7b3a1
Gravitar64 <gravitar@web.de>
2023-12-10 17:28:47 +0100

    YouTube

--- day10.py
+++ day10.py
@@ -15,17 +15,17 @@ def bfs(grid, directions, connections):
     for dx, dy in directions[grid[x, y]]:
       x2, y2 = x + dx, y + dy
       if (x2, y2) not in grid or (x2, y2) in seen: continue
-      if grid[x2, y2] not in connections[dx, dy]:  continue
+      if grid[x2, y2] not in connections[dx, dy]: continue
       queue.append((count + 1, (x2, y2)))
   return count, seen
 
 
-def is_inside(grid, x, y, size):
+def is_inside(loop, x, y, max_x):
   counter = 0
-  for _ in range(size):
+  for _ in range(max_x):
     x -= 1
     if x < 0: break
-    counter += grid.get((x, y), 'X') in '|JL'
+    counter += loop.get((x, y), '.') in '|JL'
   return counter % 2
 
 
@@ -34,19 +34,18 @@ def solve(p):
   directions = {'S': [N, S, W, E], '|': [N, S], '-': [E, W],
                 'L': [N, E], 'J': [N, W], '7': [S, W], 'F': [S, E]}
   connections = {N: {'|', '7', 'F'}, S: {'|', 'L', 'J'},
-                E: {'-', 'J', '7'}, W: {'-', 'L', 'F'}}
-
+                 E: {'-', 'J', '7'}, W: {'-', 'L', 'F'}}
   grid = {(x, y): c for y, row in enumerate(p)
           for x, c in enumerate(row) if c != '.'}
   part1, seen = bfs(grid, directions, connections)
 
   part2 = 0
   loop = {(x, y): c for (x, y), c in grid.items() if (x, y) in seen}
-  min_x, max_x = min(seen)[0], max(seen)[0]
-  min_y, max_y = min(s[1] for s in seen), max(s[1] for s in seen)
-  
-  for y in range(min_y, max_y + 1):
-    for x in range(min_x, max_x + 1):
+  min_x, max_x = min(loop)[0], max(loop)[0]
+  min_y, max_y = min(s[1] for s in loop), max(s[1] for s in loop)
+
+  for x in range(min_x, max_x + 1):
+    for y in range(min_y, max_y + 1):
       if (x, y) in loop: continue
       part2 += is_inside(loop, x, y, max_x)
 
