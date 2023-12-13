d7ec296d00233100919c5138e095ad7fd9801821
Gravitar64 <gravitar@web.de>
2023-12-13 12:19:16 +0100

    YouTube

--- day13.py
+++ day13.py
@@ -7,43 +7,42 @@ def load(file):
 
 
 def mirror(pattern):
-  for start in range(1, len(pattern)):
-    low, high = start - 1, start
-    is_mirror = True
+  for reflection in range(1, len(pattern)):
+    up, down = reflection - 1, reflection
+    mirror = True
 
-    while low >= 0 and high < len(pattern):
-      if pattern[low] != pattern[high]:
-        is_mirror = False
+    while up >= 0 and down < len(pattern):
+      if pattern[up] != pattern[down]:
+        mirror = False
         break
-      low, high = low - 1, high + 1
+      up, down = up - 1, down + 1
 
-    if is_mirror: return start
+    if mirror: return reflection
 
   return 0
 
 
-def mirror_smudge(pattern):
-  for start in range(1, len(pattern)):
-    low, high = start - 1, start
+def smudge_mirror(pattern):
+  for reflection in range(1, len(pattern)):
+    up, down = reflection - 1, reflection
     diff = 0
 
-    while low >= 0 and high < len(pattern):
-      diff += sum(a != b for a, b in zip(pattern[low], pattern[high]))
+    while up >= 0 and down < len(pattern):
+      diff += sum(a != b for a, b in zip(pattern[up], pattern[down]))
       if diff > 1: break
-      low, high = low - 1, high + 1
+      up, down = up - 1, down + 1
 
-    if diff == 1: return start
+    if diff == 1: return reflection
 
   return 0
 
 
 def solve(p):
   part1 = part2 = 0
-  patterns = [pattern.split('\n') for pattern in p]
-
-  for pattern in patterns:
+  p = [s.split('\n') for s in p]
+  for pattern in p:
     part1 += mirror(pattern) * 100 + mirror(list(zip(*pattern)))
-    part2 += mirror_smudge(pattern) * 100 + mirror_smudge(list(zip(*pattern)))
+    part2 += smudge_mirror(pattern) * 100 + smudge_mirror(list(zip(*pattern)))
 
   return part1, part2
 
