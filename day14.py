52eb3e4a842edc478f125f44970c5c58ce934f69
Gravitar64 <gravitar@web.de>
2023-12-14 11:06:45 +0100

    V2

--- day14.py
+++ day14.py
@@ -1,35 +1,48 @@
-import time, re
+import time
 
 
 def load(file):
   with open(file) as f:
-    return [row.strip() for row in f]
+    return [[c for c in row.strip()] for row in f.readlines()]
   
 
-def rolling(col):
-  for pos, rock in enumerate(col):
-    if rock != 'O': continue
-    rolling_north = pos
-    while True:
-      rolling_north -= 1
-      if rolling_north < 0: break
-      if col[rolling_north] in '#O': break
-    if rolling_north+1 != pos:
-      col[rolling_north+1] = 'O'
-      col[pos] = '.'  
-  return col  
-  
+def rolling(pattern):
+  for _ in range(4):
+    pattern = list(list(a) for a in zip(*pattern))
+    for i,col in enumerate(pattern):
+      for pos, rock in enumerate(col):
+        if rock != 'O': continue
+        rolling_north = pos
+        while True:
+          rolling_north -= 1
+          if rolling_north < 0: break
+          if col[rolling_north] in '#O': break
+        if rolling_north+1 != pos:
+          col[rolling_north+1] = 'O'
+          col[pos] = '.'  
+      pattern[i] = col
+    pattern = [row[::-1] for row in pattern]
+  return pattern      
 
+
+        
 def solve(p):
-  part1 = 0
-  p = list(zip(*p))
-  for i,col in enumerate(p):
-    p[i] = rolling(list(col))
-  for i,col in enumerate(list(zip(*p))):
-    for rock in col:
-      if rock != 'O': continue
-      part1 += len(p)-i
-  return part1    
+  hash = tuple(''.join(row) for row in p)
+  patterns, seen = [hash], set(hash)
+  counter = 0
+  while True:
+    counter += 1
+    p = rolling(p)
+    hash = tuple(''.join(row) for row in p)
+    if hash in seen: break
+    patterns.append(hash)
+    seen.add(hash)
+  offset = patterns.index(hash)
+  p = patterns[(1_000_000_000 - offset) % (counter - offset) + offset]
+  return sum(col.count('O')*(len(p)-i) for i,col in enumerate(p))
+  
+
+
     
 
 
