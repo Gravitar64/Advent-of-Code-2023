bb3d27cc9fd3d8633a992f8e5e0e58d5f5cff00e
Gravitar64 <gravitar@web.de>
2023-12-14 12:18:30 +0100

    V3

--- day14.py
+++ day14.py
@@ -3,47 +3,43 @@ import time
 
 def load(file):
   with open(file) as f:
-    return [[c for c in row.strip()] for row in f.readlines()]
-  
+    return [row.strip() for row in f.readlines()]
 
-def rolling(pattern):
+
+def roll(p):
+  p = [''.join(a) for a in zip(*p)]
+  p = ['#'.join(''.join(sorted(s, reverse=True)) for s in row.split('#')) for row in p]
+  return [''.join(a) for a in zip(*p)]
+
+
+def cycle(p):
   for _ in range(4):
-    pattern = list(list(a) for a in zip(*pattern))
-    for i,col in enumerate(pattern):
-      for pos, rock in enumerate(col):
-        if rock != 'O': continue
-        rolling_north = pos
-        while True:
-          rolling_north -= 1
-          if rolling_north < 0: break
-          if col[rolling_north] in '#O': break
-        if rolling_north+1 != pos:
-          col[rolling_north+1] = 'O'
-          col[pos] = '.'  
-      pattern[i] = col
-    pattern = [row[::-1] for row in pattern]
-  return pattern      
+    p = [''.join(a) for a in zip(*p)]
+    p = ['#'.join(''.join(sorted(s, reverse=True)) for s in row.split('#')) for row in p]
+    p = [row[::-1] for row in p]
+  return p
 
 
-        
+def total_load(p):
+  return sum(col.count('O') * (len(p) - i) for i, col in enumerate(p))
+
+
 def solve(p):
-  hash = tuple(''.join(row) for row in p)
-  patterns, seen = [hash], set(hash)
-  counter = 0
+  part1 = total_load(roll(p))
+
+  patterns, seen, counter = [p], {tuple(p)}, 0
   while True:
     counter += 1
-    p = rolling(p)
-    hash = tuple(''.join(row) for row in p)
-    if hash in seen: break
-    patterns.append(hash)
-    seen.add(hash)
-  offset = patterns.index(hash)
-  p = patterns[(1_000_000_000 - offset) % (counter - offset) + offset]
-  return sum(col.count('O')*(len(p)-i) for i,col in enumerate(p))
-  
+    p = tuple(cycle(p))
+    if p in seen: break
+    patterns.append(p)
+    seen.add(p)
 
+  offset = patterns.index(p)
+  cycle_length = counter - offset
+  part2 = total_load(patterns[(1_000_000_000 - offset) % cycle_length + offset])
 
-    
+  return part1, part2
 
 
 time_start = time.perf_counter()
