af176bb162a3653cff43a6617e398611bb18cb5f
Gravitar64 <gravitar@web.de>
2023-12-17 02:05:52 +0100

    YouTube

--- day16.py
+++ day16.py
@@ -6,53 +6,36 @@ def load(file):
     return [row.strip() for row in f]
   
 
-def follow_beam(x,y,dx,dy,p):
-  w,h = len(p[0]), len(p)
-  seen, queue = set(), [(x,y,dx,dy)]
-  while queue:
-    x,y,dx,dy = queue.pop(0)
-    x,y = x+dx, y+dy
-    if x < 0 or x >= w or y < 0 or y >= h : continue
-    ch = p[y][x]
-    if ch == '.' or ch == '-' and dx != 0 or ch == '|' and dy != 0: 
-      if (x,y,dx,dy) not in seen:
-        seen.add((x,y,dx,dy))
-        queue.append((x,y,dx,dy))
-    elif p[y][x] == '/':
-      dy,dx = -dx, -dy
-      if (x,y,dx,dy) not in seen:
-        seen.add((x,y,dx,dy))
-        queue.append((x,y,dx,dy))
-    elif p[y][x] == '\\':
-      dy,dx = dx, dy
-      if (x,y,dx,dy) not in seen:
-        seen.add((x,y,dx,dy))
-        queue.append((x,y,dx,dy))
-    else:
-      for dy,dx in [(1,0), (-1,0)] if ch == '|' else [(0,1), (0,-1)]:
-        if (x,y,dx,dy) not in seen:
-          seen.add((x,y,dx,dy))
-          queue.append((x,y,dx,dy))    
-    
-  return len({(x,y) for x,y,_,_ in seen})
-
+def follow_beam(q, g):
+  visited = set()
+  while q:
+    pos, dir = q.pop()
+    while not (pos,dir) in visited:
+      visited.add((pos,dir))
+      pos += dir
+      match g.get(pos,None):
+        case None: break
+        case '/':
+          dir = -complex(dir.imag, dir.real)
+        case '\\':
+          dir = complex(dir.imag, dir.real)
+        case '|':
+          dir = 0 + 1j
+          q.append((pos,-dir))
+        case '-':
+          dir = -1 + 0j
+          q.append((pos,-dir))
+  return len(set(pos for pos, dir in visited)) - 1
+        
 
 def solve(p):
-  part1 = follow_beam(-1,0,1,0,p)
-
-  part2 = 0
-  for row in range(len(p)):
-    part2 = max(part2, follow_beam(-1,row,1,0,p))
-    part2 = max(part2, follow_beam(len(p[0]),row,-1,0,p))
-  for col in range(len(p[0])):
-    part2 = max(part2, follow_beam(col,-1,0,1,p))
-    part2 = max(part2, follow_beam(col,len(p),0,-1,p))
-
-  return part1, part2  
-
-
-   
-
+  grid = {complex(x,y) : c for y,row in enumerate(p) for x,c in enumerate(row)}
+  part1 = follow_beam([(-1 + 0j, 1 + 0j)], grid)
+  
+  positions = ((pos - dir, dir) for dir in (1+0j, -1+0j, 0+1j, 0-1j) 
+                                for pos in grid if pos - dir not in grid)
+  part2 = max(follow_beam([(pos,dir)], grid) for pos,dir in positions)
+  return part1, part2
 
 
 time_start = time.perf_counter()
