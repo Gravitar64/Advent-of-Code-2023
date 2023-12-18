d44f99c7589b754f1ec84d2d2e09592300010aed
Gravitar64 <gravitar@web.de>
2023-12-18 11:07:56 +0100

    V2

--- day18.py
+++ day18.py
@@ -1,4 +1,4 @@
-import time
+import time, shapely
 
 
 def load(file):
@@ -6,45 +6,23 @@ def load(file):
     return [row.strip().split() for row in f]
   
 
-def flood_fill(trenches):
-  x,y = min(trenches)
-  dx,dy = 1,1
-  while True:
-    x,y = x+dx, y+dy
-    if (x,y) in trenches: continue
-    break
-
-  q = [(x,y)]
-  while q:
-    x,y = q.pop(0)
-    trenches.add((x,y))
-    for x2,y2 in ((x+1,y), (x-1,y), (x,y+1), (x,y-1)):
-      if (x2,y2) in trenches: continue
-      q.append((x2,y2))
-      trenches.add((x2,y2))
-  return trenches
-    
-    
 def solve(p,part1):
   dirs = dict(R=(1,0), D=(0,1), L=(-1,0), U=(0,-1))
   dirs2 = [dir for dir in dirs.values()]
   x,y = 0,0
-  trenches = {(x,y)}
+  
+  corners = [(x,y)]
   for dir, l, color in p:
     if part1:
       dx,dy = dirs[dir]
-      for _ in range(int(l)):
-        x,y = x+dx, y+dy
-        trenches.add((x,y))
     else:
       l = int(color[2:-2],16)
       dx,dy = dirs2[int(color[-2])]
-      for _ in range(int(l)):
-        x,y = x+dx, y+dy
-        trenches.add((x,y))  
-
-  trenches = flood_fill(trenches)
-  return len(trenches)
+    x,y = x+dx*int(l), y+dy*int(l)
+    corners.append((x,y))
+       
+  pgon = shapely.Polygon(corners).buffer(0.5, join_style='mitre')
+  return int(pgon.area)
     
 
 time_start = time.perf_counter()
