3201b2552d5d12d0614ced5e7da1b7584b336665
Gravitar64 <gravitar@web.de>
2023-12-24 13:11:31 +0100

    V0.4

--- day22.py
+++ day22.py
@@ -6,7 +6,7 @@ def load(file):
     return [list(map(int,re.findall('\d+',row))) for row in f.readlines()]
 
 
-def fall(segments):
+def settle(segments):
   bricks = set((z,x,y) for segment in segments for z,x,y in segment)
   for i,segment in enumerate(segments):
     falling = True
@@ -27,6 +27,30 @@ def fall(segments):
   return segments
 
 
+def can_disintegrated(segments):
+  counter = 0
+  segs = []
+  for segment in segments:
+    maxZ = max(z for z,_,_ in segment)
+    bricks = set((z,x,y) for s in segments for z,x,y in s if s != segment)
+    above = [s for s in segments if any(z == maxZ+1 for z,x,y in s)]
+    if not above:
+      counter += 1
+      segs.append(segment)
+      continue
+    can_fall = False
+    for segment2 in above:
+      if can_fall: break
+      for (z,x,y) in segment2:
+        new_brick = z-1,x,y
+        if new_brick in (bricks - {(z,x,y) for z,x,y in segment2}): break
+      else:
+        can_fall = True  
+    if not can_fall:
+      segs.append(segment)
+      counter += 1
+  return counter, segs  
+
 def solve(p):
   segments = []
   for x1,y1,z1,x2,y2,z2 in p:
@@ -36,9 +60,8 @@ def solve(p):
         for y in range(y1,y2+1):
           segment.append((z,x,y))
     segments.append(segment)      
-  print(sorted(segments))
-  segments = fall(sorted(segments))
-  print(segments)
+  segments = settle(sorted(segments))
+  return can_disintegrated(segments)
     
 
 
