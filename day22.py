00f87233a1a455a1dc7b9f864c4153ee0a772d0e
Gravitar64 <gravitar@web.de>
2023-12-24 12:21:01 +0100

    V0.3

--- day22.py
+++ day22.py
@@ -1,30 +1,51 @@
-import time, re 
-from skspatial.objects import LineSegment, Vector
-import itertools
+import time, re
 
 
 def load(file):
   with open(file) as f:
     return [list(map(int,re.findall('\d+',row))) for row in f.readlines()]
-  
+
+
+def fall(segments):
+  bricks = set((z,x,y) for segment in segments for z,x,y in segment)
+  for i,segment in enumerate(segments):
+    falling = True
+    while falling:
+      for (z,x,y) in segment:
+        if z < 2: 
+          falling = False
+          break
+        new_brick = z-1,x,y
+        if new_brick in (bricks - {(z,x,y) for z,x,y in segment}): 
+          falling = False
+          break
+      else:
+        segment_new = [(z-1,x,y) for z,x,y in segment]
+        segments[i] = segment_new
+        bricks = bricks - {(z,x,y) for z,x,y in segment} | {(z,x,y) for z,x,y in segments[i]}
+        segment = segment_new
+  return segments
+
 
 def solve(p):
-  lines = [LineSegment(coords[:3], coords[3:]) for coords in p]
-  gravity = Vector([0,0,-1])
-  for a,b in itertools.combinations(lines,2):
-    a = LineSegment(a.point_a+gravity, a.point_b+gravity)
-    try:
-      a.intersect_line_segment(b)
-      print(a,b)
-    except:
-      print('No Intersection')  
+  segments = []
+  for x1,y1,z1,x2,y2,z2 in p:
+    segment = []
+    for z in range(z1,z2+1):
+      for x in range(x1,x2+1):
+        for y in range(y1,y2+1):
+          segment.append((z,x,y))
+    segments.append(segment)      
+  print(sorted(segments))
+  segments = fall(sorted(segments))
+  print(segments)
+    
 
-  for a,b in itertools.combinations(lines,2):
-    try:
-      print(a.intersect_line_segment(b))
-      print(a,b)
-    except:
-      pass
+
+
+
+
+  
 
 
 time_start = time.perf_counter()
