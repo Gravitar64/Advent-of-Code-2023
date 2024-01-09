3b4864046b8e72ebc2a9761135430b786e27d2a1
Gravitar64 <gravitar@web.de>
2024-01-09 17:27:16 +0100

    V4

--- day20.py
+++ day20.py
@@ -1,61 +1,57 @@
-import time, re, math
+import time, re, math, collections
 
 
 def load(file):
   with open(file) as f:
-    return [re.findall('%?&?\w+',row) for row in f.readlines()]
+    return [re.findall('%?&?\w+',row) for row in f]
   
 
-def build_modules(p):
-  modules = dict()
+def solve(p):
+  modules, modules_inv = dict(), collections.defaultdict(set)
+  TYPE, RECEIVER, STATE = range(3)
   for sender, *receiver in p:
-    name, type = sender[1:], sender[0]
+    type, sender = sender[0], sender[1:]
     match type:
       case '%': state = False
       case '&': state = dict()
       case 'b': state = None
-    modules[name] = [name, type, receiver, state]
-  for sender,_,receiver,_ in modules.values():
+    modules[sender] = [type, receiver, state]
     for rec in receiver:
-      if rec not in modules: continue
-      if modules[rec][TYPE] != '&': continue
-      modules[rec][STATE][sender] = False
-  return modules
-
-
-def solve(p):
-  modules = build_modules(p)
-  BC = 'roadcaster'
-  LowHigh = [0,0]
-  main_conjunction = [module[SENDER] for module in modules.values() if 'rx' in module[RECEIVER]][0]
-  cycle = {sender:0 for sender in modules[main_conjunction][STATE]}
-  for button in range(1,10_000):
-    if all(cycle.values()): break
-    fifo = [('Button', BC, False)]
+      modules_inv[rec].add(sender)
+  
+  for rec, sender in modules_inv.items():
+    if rec not in modules: continue
+    if modules[rec][TYPE] != '&': continue
+    modules[rec][STATE]={s:False for s in sender}
+    
+  master_conjunction = modules_inv['rx'].pop()
+  cycle = dict()
+     
+  lowHigh = [0,0]
+  for button in range(10_000):
+    fifo = [('Button', 'roadcaster', False)]
     while fifo:
       sender, receiver, impulse = fifo.pop(0)
-      if button < 1001: LowHigh[impulse] += 1
+      if button < 1000: lowHigh[impulse] += 1
       if receiver not in modules: continue
-      
-      if receiver == main_conjunction and impulse:
-        cycle[sender] = button - cycle[sender]  
-      
+
+      if receiver == master_conjunction and impulse:
+        cycle[sender] = button - cycle.get(sender,0)
+
       if modules[receiver][TYPE] == '%':
         if impulse: continue
-        modules[receiver][STATE] = not modules[receiver][STATE]
-        impulse = modules[receiver][STATE]
-      
+        impulse = modules[receiver][STATE] = not modules[receiver][STATE]
+   
       if modules[receiver][TYPE] == '&':
         modules[receiver][STATE][sender] = impulse
         impulse = not all(modules[receiver][STATE].values())
-      
-      for nxt in modules[receiver][RECEIVER]:
-        fifo.append((receiver,nxt,impulse))
-  
-  return math.prod(LowHigh), math.lcm(*cycle.values())
-  
+
+      for receiver_receiver in modules[receiver][RECEIVER]:
+        fifo.append((receiver, receiver_receiver, impulse))
+
+  return math.prod(lowHigh), math.lcm(*cycle.values())
+
 
 time_start = time.perf_counter()
-SENDER, TYPE, RECEIVER, STATE = range(4)
 print(f'Part 1: {solve(load("day20.txt"))}')
 print(f'Solved in {time.perf_counter()-time_start:.5f} Sec.')
