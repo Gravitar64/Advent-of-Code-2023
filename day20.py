6947e5c3c2a6629ca59e4b03102335a73fd0aa17
Gravitar64 <gravitar@web.de>
2024-01-10 16:04:40 +0100

    YouTube2

--- day20.py
+++ day20.py
@@ -1,33 +1,33 @@
-import time, re, math, collections
+import time, re, collections, math
 
 
 def load(file):
   with open(file) as f:
-    return [re.findall('%?&?\w+',row) for row in f]
+    return [re.findall('%?&?\w+', row) for row in f]
   
 
 def solve(p):
-  modules, modules_inv = dict(), collections.defaultdict(set)
   TYPE, RECEIVER, STATE = range(3)
+  modules, modules_i = dict(), collections.defaultdict(set)
   for sender, *receiver in p:
     type, sender = sender[0], sender[1:]
     match type:
       case '%': state = False
       case '&': state = dict()
       case 'b': state = None
-    modules[sender] = [type,receiver,state]
+    modules[sender] = [type, receiver, state]
     for rec in receiver:
-      modules_inv[rec].add(sender)
-
-  for rec, sender in modules_inv.items():
+      modules_i[rec].add(sender)
+  
+  for rec, senders in modules_i.items():
     if rec not in modules: continue
     if modules[rec][TYPE] != '&': continue
-    modules[rec][STATE] = {s:False for s in sender}
-
-  lowHigh = [0,0]
-  master_conjunction = modules_inv['rx'].pop()
+    modules[rec][STATE] = {s:False for s in senders}
+  
+  master_conjunction = modules_i['rx'].pop()
   cycles = dict()
- 
+  
+  lowHigh = [0,0]
   for button in range(1,10_000):
     fifo = [('button', 'roadcaster', False)]
     while fifo:
@@ -38,18 +38,31 @@ def solve(p):
       if receiver == master_conjunction and pulse:
         cycles[sender] = button - cycles.get(sender,0)
         
+
       if modules[receiver][TYPE] == '%':
         if pulse: continue
         pulse = modules[receiver][STATE] = not modules[receiver][STATE]
-
+      
       if modules[receiver][TYPE] == '&':
         modules[receiver][STATE][sender] = pulse
-        pulse = not all(modules[receiver][STATE].values())  
+        pulse = not all(modules[receiver][STATE].values())
 
-      for receiver_receiver in modules[receiver][RECEIVER]:
-        fifo.append((receiver, receiver_receiver, pulse))
+      for nxt in modules[receiver][RECEIVER]:
+        fifo.append((receiver, nxt, pulse))
+  return math.prod(lowHigh), math.lcm(*cycles.values())        
 
-  return math.prod(lowHigh), math.lcm(*cycles.values())
+
+
+
+
+
+
+
+
+
+  return modules
+       
+     
 
 
 time_start = time.perf_counter()
