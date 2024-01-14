c2fb4816613f16574cd530ed70a63c20c02bfd97
Gravitar64 <gravitar@web.de>
2024-01-14 19:45:47 +0100

    V3

--- day21.py
+++ day21.py
@@ -1,29 +1,28 @@
-import time
+import time, numpy
 
 
 def load(file):
   with open(file) as f:
     return [row.strip() for row in f]
-  
+
 
 def solve(p):
-  gardens = {(x,y) for y,row in enumerate(p) for x,c in enumerate(row) if c != '#'}
-  size = len(p) 
-  S = (size//2, size//2) #allways quadratic, allways in the centre
+  size = len(p)
+  walls = {(x, y) for y, row in enumerate(p) for x, c in enumerate(row) if c == '#'}
+  nodes, results = {(size//2, size//2)}, [1]
+  neig = lambda x,y: [(x+1,y), (x-1,y), (x,y+1), (x,y-1)]
   
-  k,r =divmod(26501365,size)
-  visited, new, cache = {S}, {S}, {0:1}
+  for _ in range(1, 65+131*2+1):
+    nodes = {(x2,y2) for x,y in nodes for x2,y2 in neig(x,y) if (x2%size,y2%size) not in walls}
+    results.append(len(nodes))
 
-  for n in range(1,2*size+r+1): #simulate till reached end off 2nd tile to the right
-    visited, new = new, {(x2,y2) for x,y in new for x2,y2 in ((x+1,y), (x-1,y), (x,y+1), (x,y-1))
-                         if (x2,y2) not in visited and (x2%size, y2%size) in gardens}
-    cache[n] = len(new) + (cache[n-2] if n > 1 else 0)
+  part1 = results[64]
   
-  d1 = cache[2*size+r] - cache[r+size]
-  d2 = cache[2*size+r] + cache[r] - 2*cache[r+size]
-  part1 = cache[64]
-  part2 = cache[2*size+r]+(k-2)*(2*d1+(k-1)*d2)//2
-
+  points = [results[n] for n in (65, 65 + 131, 65 + 2 * 131)]
+  x = 26501365 // size
+  a, b, c = map(round, numpy.poly1d(numpy.polyfit([0, 1, 2], points, 2)))
+  part2 = a * x**2 + b * x + c
+  
   return part1, part2
 
 
